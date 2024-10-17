d3466724748cbda6762e1c472b4f5eafb57978c2
Tommy <60412916+tommy-donavon@users.noreply.github.com>
2024-10-16 21:11:18 -0600

    feat: darwin port (#17)
    * feat: wip darwin port
    
    * feat: added some packages / cleanup
    
    * feat: darwin port complete
    
    * chore: update flake

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     snowfall-lib = {
       url = "github:snowfallorg/lib";
